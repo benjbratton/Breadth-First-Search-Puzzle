977c254ecbd2d1196beac8ff381db70aac78e8c7
Ben Bratton <36572589+benjbratton@users.noreply.github.com>
2018-04-29 16:33:28 -0400

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,19 @@
+package application;
+import java.util.HashMap;
+import java.util.Vector;
+
+
+
+	
+public class Solution {
+	
+
+	/******************************************   Implementation Here  ***************************************/
+
+	/*			Implementation here: you need to implement the Breadth First Search Method				 				*/
+	/*			Please refer the instruction document for this function in details								     	*/
+	public static void breadthFirstSearch(int[] num, int m, Vector solution)
+	{
+		
+	}
+}
