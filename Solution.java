7206577ac8affb67ea4d3ae30e63548dba768a0f
Ben Bratton <36572589+benjbratton@users.noreply.github.com>
2018-04-29 17:28:32 -0400

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -16,4 +16,32 @@ public class Solution {
 	{
 		
 	}
+	
+		//This method takes the current index of the empty space in the puzzle (m) and returns the index of the puzzle piece on top of it.
+	//If the empty puzzle piece is on the top edge the method will return a -1
+	public int topNeighbor(int m) {
+		if (m < 2 || m > 7) return -1;
+		else return m - 3;
+	}//end topNeighbor
+	
+	//This method takes the current index of the empty space in the puzzle (m) and returns the index of the puzzle piece just below it.
+	//If the empty puzzle piece is on the bottom edge the method will return a -1
+	public int bottomNeighbor(int m) {
+		if (m < 0 || m > 4) return -1;
+		else return m + 3;
+	}//end bottomNeighbor
+	
+	//This method takes the current index of the empty space in the puzzle (m) and returns the index of the puzzle piece to its left.
+	//If the empty puzzle piece is on the left edge the method will return a -1
+	public int leftNeighbor(int m){
+		if (m % 3 == 1) return -1;
+		else return m - 1;
+	}//end leftNeighbor
+	
+	//This method takes the current index of the empty space in the puzzle (m) and returns the index of the puzzle piece to its right.
+	//If the empty puzzle piece is on the right edge the method will return a -1
+	public int rightNeighbor(int m) {
+		if (m % 3 == 0) return -1;
+		else return m + 1;
+	}//end rightNeighbor
 }
