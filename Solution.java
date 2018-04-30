3b09ca61c290a5180a4c5a8d60d73b485842580f
caglen2 <36609665+caglen2@users.noreply.github.com>
2018-04-29 22:26:58 -0400

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -15,50 +15,64 @@ public class Solution {
 	public static void breadthFirstSearch(int[] num, int m, Vector solution)
 	{
 		Queue<Integer> q = new LinkedList<Integer>();
-		q.add(m);
-		if(topNeighbor(m) != -1)
-			swap(num, m, m+3);
-		if(bottomNeighbor(m) != -1)
-			swap(num, m, m-3);
-		if(leftNeighbor(m) != -1)
-			swap(num, m, m+1);
-		if(rightNeighbor(m) != -1)
-			swap(num, m, m-1);
+		if(topNeighbor(m) != -1) {
+			int a = swap(num, m, m+3);
+			q.add(a);
+			System.out.println("a = " + a + "\t m = " + m);
+		}
+		if(bottomNeighbor(m) != -1) {
+			int b = swap(num, m, m-3);
+			q.add(b);
+			System.out.println("b = " + b);
+		}
+			
+		if(leftNeighbor(m) != -1) {
+			int c = swap(num, m, m+1);
+			q.add(c);
+			System.out.println("c = " + c);
+		}
+			
+		if(rightNeighbor(m) != -1) {
+			int d = swap(num, m, m-1);
+			q.add(d);
+			System.out.println("d = " + d);
+		}
+			
 		
 	}
-	
-		//This method takes the current index of the empty space in the puzzle (m) and returns the index of the puzzle piece on top of it.
-	//If the empty puzzle piece is on the top edge the method will return a -1
-	public int topNeighbor(int m) {
-		if (m < 2 || m > 7) return -1;
-		else return m - 3;
-	}//end topNeighbor
-	
-	//This method takes the current index of the empty space in the puzzle (m) and returns the index of the puzzle piece just below it.
-	//If the empty puzzle piece is on the bottom edge the method will return a -1
-	public int bottomNeighbor(int m) {
-		if (m < 0 || m > 4) return -1;
-		else return m + 3;
-	}//end bottomNeighbor
-	
-	//This method takes the current index of the empty space in the puzzle (m) and returns the index of the puzzle piece to its left.
-	//If the empty puzzle piece is on the left edge the method will return a -1
-	public int leftNeighbor(int m){
-		if (m % 3 == 1) return -1;
-		else return m - 1;
-	}//end leftNeighbor
-	
-	//This method takes the current index of the empty space in the puzzle (m) and returns the index of the puzzle piece to its right.
-	//If the empty puzzle piece is on the right edge the method will return a -1
-	public int rightNeighbor(int m) {
-		if (m % 3 == 0) return -1;
-		else return m + 1;
-	}//end rightNeighbor
-	public static void swap(int[] num, int m, int r) {
-		int temp = num[m];
-		num[m] = num[r];
-		num[r] = temp;
-	}
+	//This method takes the current index of the empty space in the puzzle (m) and returns the index of the puzzle piece on top of it.
+		//If the empty puzzle piece is on the top edge the method will return a -1
+		public static int topNeighbor(int m) {
+			if (m == 0 || m == 1 || m == 2) return -1;
+			else return m - 3;
+		}//end topNeighbor
+		
+		//This method takes the current index of the empty space in the puzzle (m) and returns the index of the puzzle piece just below it.
+		//If the empty puzzle piece is on the bottom edge the method will return a -1
+		public static int bottomNeighbor(int m) {
+			if (m == 6 || m == 7 || m == 8) return -1;
+			else return m + 3;
+		}//end bottomNeighbor
+		
+		//This method takes the current index of the empty space in the puzzle (m) and returns the index of the puzzle piece to its left.
+		//If the empty puzzle piece is on the left edge the method will return a -1
+		public static int leftNeighbor(int m){
+			if (m == 0 || m == 3 || m == 6) return -1;
+			else return m - 1;
+		}//end leftNeighbor
+		
+		//This method takes the current index of the empty space in the puzzle (m) and returns the index of the puzzle piece to its right.
+		//If the empty puzzle piece is on the right edge the method will return a -1
+		public static int rightNeighbor(int m) {
+			if (m == 2 || m == 5 || m == 8) return -1;
+			else return m + 1;
+		}//end rightNeighbor
+		public static int swap(int[] num, int m, int r) {
+			int temp = num[m];
+			num[m] = num[r];
+			num[r] = temp;
+			return num[m];
+		}
 	
 	//This method returns the integer of the neighbor that is closest to the index of the best piece to be swapped with the empty space to create a working path.
 	//If the index of a neighbor is out of the puzzle (i.e index = -1) then the comparison will always be greater than m's current location and therefore not a valid swap.
