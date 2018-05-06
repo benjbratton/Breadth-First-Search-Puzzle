2f7dccb163277d4eadca1f4b0ef92ced9afd73bd
Ben Bratton <36572589+benjbratton@users.noreply.github.com>
2018-05-05 23:44:37 -0400

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -4,54 +4,50 @@ import java.util.Vector;
 	
 public class Solution {	
 
-/******************************************   Implementation Here  ***************************************/
-public static boolean[] visited= new boolean[9];//Creates an array that will hold the values that have been visited
-private static int[] edgeTo = new int[9];//Creates an array that will hold the values with edges connected to the missing value
-private static int[] distTo = new int[9];//Creates an array that will holds the path to each value
-
-/*		Implementation here: you need to implement the Breadth First Search Method				 				*/
-/*		Please refer the instruction document for this function in details								     	*/
-
-public static void breadthFirstSearch(int[] num, int m, Vector solution)
-{
-	Queue<Integer> q = new LinkedList<Integer>(); //Creates a queue to perform the breadth first search 
-	PuzzleGrid newPuzzle = new PuzzleGrid(num, m); //Creates an object of the class PuzzleGrid
-	q.add(m);
-	visited[m] = true;
-	distTo[m] = 0;
-	while(!q.isEmpty()) { //Checks if the queue is empty. If it isn't it performs the breadth first search
-		int a = q.remove();
-		for(int b : newPuzzle.grid) {//Checks to see if there are adjacent tiles to the missing tile
-			if(!visited[b]) {//Checks if the adjacent tiles have already been visited or not
-				
-				//Check to see the values that are adjacent to the missing value
-				//Swaps the array values
-				if(newPuzzle.topNeighbor() != null)//If there is a topNeighbor, add that value to the queue
-					q.add(b);
-				if(newPuzzle.bottomNeighbor() != null)//If there is a bottomNeighbor, add that value to the queue
-					q.add(b);
-				if(newPuzzle.leftNeighbor() != null)//If there is a leftNeighbor, add that value to the queue
-					q.add(b);
-				if(newPuzzle.rightNeighbor() != null)//If there is a RightNeighbor, add that value to the queue
-					q.add(b);
-				
-				visited[b] = true;
-				edgeTo[b] = a;
-				distTo[b] = distTo[a]+1;
-				
-				// Add all the values from the queue into the vector "solution"
-				solution.add(q.poll());
+	/******************************************   Implementation Here  ***************************************/
+	public static boolean[] visited= new boolean[9];
+	private static int[] edgeTo = new int[9];
+	private static int[] distTo = new int[9];
+	/*			Implementation here: you need to implement the Breadth First Search Method				 				*/
+	/*			Please refer the instruction document for this function in details								     	*/
+	public static void breadthFirstSearch(int[] num, int m, Vector<Integer> solution)
+	{
+		Queue<Integer> q = new LinkedList<Integer>();
+		PuzzleGrid newPuzzle = new PuzzleGrid(num, m);
+		q.add(m);
+		visited[m] = true;
+		distTo[m] = 0;
+		while(!q.isEmpty()) {
+			int a = q.remove();
+			for(int b : newPuzzle.grid) {
+				if(!visited[b]) {
+					if(newPuzzle.topNeighbor() != null)
+						q.add(b);
+					if(newPuzzle.bottomNeighbor() != null)
+						q.add(b);
+					if(newPuzzle.leftNeighbor() != null)
+						q.add(b);
+					if(newPuzzle.rightNeighbor() != null)
+						q.add(b);
+					//q.add(b);
+					visited[b] = true;
+					edgeTo[b] = a;
+					distTo[b] = distTo[a]+1;
+					solution.add(a);
+				}
+			}	
+		}
 		
-	//sill needs th breadth first search
+		//sill needs th breadth first search
 		
-       	// Add all the values from the queue into the vector "solution"
+        	// Add all the values from the queue into the vector "solution"
 		while (!q.isEmpty()) {
-           		solution.add(q.poll());
+            		solution.add(q.poll());
         	}
 		
 		
-    	}//end breadthFirstSearch
-}//end class Solution
+    	} //end breadthFirstSearch
+
 
 
 	//We don't need these two to get it to work. We just need to figure out how the switch with puzzle grid works
@@ -76,4 +72,4 @@ public static void breadthFirstSearch(int[] num, int m, Vector solution)
 		}
 		return -1;
 	}//end IndexOfM
-}//end methods class 
+}//end class Solution
